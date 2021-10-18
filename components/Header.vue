120285248d20d47d56b46f309b9384a402e4a80a
visudo <visudo20179C@protonmail.com>
2021-10-18 16:41:32 -0400

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="flex justify-between h-24 bg-gray-900 border-double border-b-8 border-gray-500">
+		<div class="flex mt-auto mb-auto">
+			<img class="h-12 w-12 ml-4 mt-auto mb-auto" src="../static/favicon.svg"/>
+			<div class="text-lg text-visudo-green font-bold mt-auto mb-auto ml-2"> Streamer & Web Developer </div>
+		</div>
+		<div class="flex mt-auto mb-auto text-lg text-blue-600 font-bold">
+			<a class="mr-2 hover:text-blue-300" href="https://www.twitch.tv/v1sudo" target="_blank">Twitch</a> |
+			<a class="ml-2 mr-2 hover:text-blue-300" href="https://www.twitter.com/v1sudo" target="_blank">Twitter</a> |
+			<a class="ml-2 mr-2 hover:text-blue-300" href="https://www.youtube.com/channel/UC4gq_UflDu--JuISXL4C8UQ" target="_blank">YouTube</a> |
+			<a class="ml-2 mr-4 hover:text-blue-300" href="https://github.com/visudo20179C" target="_blank">GitHub</a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+		colors: {
+			'visudo-red': '#E74856',
+			'visudo-green': '#15AB0D',
+			'visudo-blue': '#2668FF',
+			'visudo-orange': '#E95420',
+		}
+	},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
