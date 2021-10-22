0b7312b7255e963c06b94e3122ac1a5ca3fcd2d4
visudo <visudo20179C@protonmail.com>
2021-10-22 14:47:41 -0400

    verbiage updates

--- components/Main.vue
+++ components/Main.vue
@@ -7,11 +7,11 @@
 				</div>
 				<div class="mb-24 w-3/4 m-auto">
 					<transition name="fade" mode="out-in">
-						<div class="text-gray-900 text-center" v-if="!firstShow" key="0">
+						<div class="text-gray-900 text-left" v-if="!firstShow" key="0">
 							Hi, my name is Cameron, otherwise known as my online alias "visudo". I'm a full-stack web developer who has been doing web development work for 3+ years, mostly self-taught. Why do I call myself "visudo"?
 						</div>
-						<div class="text-gray-900 text-center" v-else-if="firstShow" key="1">
-							It's based upon the Linux command visudo. visudo locks the sudoers file against multiple simultaneous edits, provides basic sanity checks, and checks for parse errors. The sudoers file controls who can run commands as the root user of the OS. I thought it would look cool as an IGN.
+						<div class="text-gray-900 text-left" v-else-if="firstShow" key="1">
+							It's based upon the Linux command visudo. visudo locks the sudoers file against multiple simultaneous edits, provides basic sanity checks, and checks for parse errors. The sudoers file controls who can run commands as the root user of the OS.
 						</div>
 					</transition>
 				</div>
@@ -38,10 +38,10 @@
 				</div>
 				<div class="mt-8 mb-24 w-3/4 m-auto">
 					<transition name="fade" mode="out-in">
-						<div class="text-gray-900 text-center" v-if="!secondShow" key="0">
-							I want to develop websites and web services for Streamers and Content Creators alike. The benefit of hiring me compared to going with a flat site generator is that you get a completely customized experience. I make dynamic sites that are tailored to your style and have custom features.
+						<div class="text-gray-900 text-left" v-if="!secondShow" key="0">
+							I want to develop websites and web services for Streamers and Content Creators alike. The benefit of hiring me compared to going with a competitor is that you get a completely customized experience. I make dynamic sites that are rich with style and custom features.
 						</div>
-						<div class="text-gray-900 text-center" v-else-if="secondShow" key="1">
+						<div class="text-gray-900 text-left" v-else-if="secondShow" key="1">
 							I also have extensive knowledge using 3rd-party API's such as PayPal, Twitch, Discord, and more. This gives me the oppourtunity to add custom features to your website and interact with other services that you use that would otherwise be unavailable.
 						</div>
 					</transition>
@@ -73,9 +73,9 @@
 					I've worked on several websites and web projects. My core langauges are PHP &amp; JavaScript. I've used these languages to implement standalone projects as well as projects using a framework such as Laravel or VueJS. Here are some examples:
 				</div>
 				<div class="text-gray-900 text-left ml-2 w-5/6 ml-auto mr-auto text-sm mb-12 pt-8 flex flex-col justify-between sm:flex-row">
-					<div class="flex flex-col"><a href="https://staging.ponder-app.org/" target="_blank" class="w-2/3 ml-auto mr-auto text-gray-900 text-center text-xs sm:text-md border-2 rounded-full py-1.5 px-3 border-gray-900 text-md font-black hover:text-visudo-blue hover:border-visudo-blue transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60">Ponder</a><div class="w-2/3 m-auto mt-4 mb-4 text-center">An API-based Trivia Appdivcation written in the Laravel framework.</div></div>
+					<div class="flex flex-col"><a href="https://staging.ponder-app.org/" target="_blank" class="w-2/3 ml-auto mr-auto text-gray-900 text-center text-xs sm:text-md border-2 rounded-full py-1.5 px-3 border-gray-900 text-md font-black hover:text-visudo-blue hover:border-visudo-blue transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60">Ponder</a><div class="w-2/3 m-auto mt-4 mb-4 text-center">An API-based Trivia Application written in the Laravel framework.</div></div>
 					<div class="flex flex-col"><a href="https://connect-four.visudo.me/" target="_blank" class="w-2/3 ml-auto mr-auto text-gray-900 text-center text-xs sm:text-md border-2 rounded-full py-1.5 px-3 border-gray-900 text-md font-black hover:text-visudo-blue hover:border-visudo-blue transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60">ConnectFour</a><div class="w-2/3 m-auto mt-4 mb-4 text-center">A web-based version of the game "Connect-Four" in the VueJS framework.<a href="https://github.com/visudo20179C/connect-four" target="_blank"><img src="../static/GitHub-Mark-32px.png" class="m-auto mt-4"/></a></div></div>
-					<div class="flex flex-col"><a href="https://apex-stats.visudo.me/" target="_blank" class="w-2/3 ml-auto mr-auto text-gray-900 text-center text-xs sm:text-md border-2 rounded-full py-1.5 px-3 border-gray-900 text-md font-black hover:text-visudo-blue hover:border-visudo-blue transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60">ApexPlayerStats</a><div class="w-2/3 m-auto mt-4 text-center"> - A hobby project that was inspired by my love for gaming. Another in VueJS.<a href="https://github.com/visudo20179C/apex-player-stats" target="_blank"><img src="../static/GitHub-Mark-32px.png" class="m-auto mt-4"/></a></div></div>
+					<div class="flex flex-col"><a href="https://apex-stats.visudo.me/" target="_blank" class="w-2/3 ml-auto mr-auto text-gray-900 text-center text-xs sm:text-md border-2 rounded-full py-1.5 px-3 border-gray-900 text-md font-black hover:text-visudo-blue hover:border-visudo-blue transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60">ApexPlayerStats</a><div class="w-2/3 m-auto mt-4 text-center">A hobby project that was inspired by my love for gaming. Another in VueJS.<a href="https://github.com/visudo20179C/apex-player-stats" target="_blank"><img src="../static/GitHub-Mark-32px.png" class="m-auto mt-4"/></a></div></div>
 				</div>
 			</div>
 		</div>
