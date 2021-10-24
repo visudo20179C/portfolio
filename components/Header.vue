06c9573d47260fdc65e8c1f6c525a79dff72c89a
visudo <visudo20179C@protonmail.com>
2021-10-24 12:13:55 -0400

    updates

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
 	<div class="flex justify-between h-24 bg-gray-900 border-double border-b-8 border-gray-500">
 		<div class="flex mt-auto mb-auto">
 			<img class="h-12 w-12 ml-4 mt-auto mb-auto" src="../static/favicon.svg"/>
-			<div class="text-lg text-visudo-green font-bold mt-auto mb-auto ml-2"> Streamer & Web Developer </div>
+			<div class="text-lg text-visudo-green font-bold mt-auto mb-auto ml-2 animate-pulse"> Streamer & Web Developer </div>
 		</div>
 		<div class="flex mt-auto mb-auto text-lg text-blue-600 font-bold">
 			<a class="mr-2 hover:text-blue-300 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60" href="https://www.twitch.tv/v1sudo" target="_blank">Twitch</a> |
--- components/Main.vue
+++ components/Main.vue
@@ -1,70 +1,44 @@
 <template class="bg-gray-900">
 	<div>
-		<div class="flex flex-col bg-gray-900 h-full w-full justify-center items-center sm:flex-row">
-			<div class="w-2/3 h-5/6 mt-10 mb-10 ml-4 bg-gray-500 border rounded-lg border-gray-900 shadow-lg relative sm:w-1/3">
-				<div class="w-2/3 text-2xl text-visudo-green font-black mb-8 mt-4 border rounded-lg bg-gray-900 border-gray-900 text-center ml-auto mr-auto">
-					Who Am I?
-				</div>
-				<div class="mb-24 w-3/4 m-auto">
-					<transition name="fade" mode="out-in">
-						<div class="text-gray-900 text-left" v-if="!firstShow" key="0">
-							Hi, my name is Cameron, otherwise known as my online alias "visudo". I'm a full-stack web developer who has been doing web development work for 3+ years, mostly self-taught. Why do I call myself "visudo"?
+		<div class="w-2/3 h-5/6 mt-20 mb-20 mx-auto bg-gray-900 border-b-4 border-double border-gray-500 shadow-lg relative sm:h-80">
+			<div class="mb-24 w-3/4 m-auto">
+				<transition name="fade" mode="out-in">
+					<div v-if="!firstShow" key="0">
+						<div class="w-2/3 text-3xl text-visudo-green font-black mb-8 mt-4 border rounded-lg bg-gray-900 border-gray-900 text-center ml-auto mr-auto">
+							Who am I?
 						</div>
-						<div class="text-gray-900 text-left" v-else-if="firstShow" key="1">
-							It's based upon the Linux command visudo. visudo locks the sudoers file against multiple simultaneous edits, provides basic sanity checks, and checks for parse errors. The sudoers file controls who can run commands as the root user of the OS.
-						</div>
-					</transition>
-				</div>
-				<div class="pb-8">
-					<div class="absolute bottom-10 left-10 right-10">
-						<div class="flex justify-center">
-							<button @click="firstShowPrev">
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-900 hover:text-visudo-blue" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
-								</svg>
-							</button>
-							<button @click="firstShowNext">
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-900 hover:text-visudo-blue" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-								</svg>
-							</button>
+						<div class="text-gray-500 text-lg font-bold">
+							Hi, my name is Cameron, otherwise known as my online alias "visudo". I'm a full-stack web developer who has been doing web development work for 3+ years, mostly self-taught. I use PHP, JavaScript, HTML &amp; CSS.
 						</div>
 					</div>
-				</div>
+					<div class="text-gray-900 text-left" v-else-if="firstShow" key="1">
+						<div class="w-2/3 text-3xl text-visudo-green font-black mb-8 mt-4 border rounded-lg bg-gray-900 border-gray-900 text-center ml-auto mr-auto">
+							What do I want to do?
+						</div>
+						<div class="text-gray-500 text-lg font-bold">
+							I want to develop websites and web services for Streamers and Content Creators alike. The benefit of hiring me compared to going with a competitor is that you get a completely customized experience. I also have extensive knowledge using 3rd-party API's such as PayPal, Twitch, Discord, and more.
+						</div>
+					</div>
+				</transition>
 			</div>
-			<div class="w-2/3 h-5/6 mt-10 mb-10 ml-4 bg-gray-500 border rounded-lg border-gray-900 shadow-lg relative sm:w-1/3">
-				<div class="w-2/3 text-2xl text-visudo-green font-black mb-4 mt-4 border rounded-lg bg-gray-900 border-gray-900 text-center ml-auto mr-auto">
-					What do I want to do?
-				</div>
-				<div class="mt-8 mb-24 w-3/4 m-auto">
-					<transition name="fade" mode="out-in">
-						<div class="text-gray-900 text-left" v-if="!secondShow" key="0">
-							I want to develop websites and web services for Streamers and Content Creators alike. The benefit of hiring me compared to going with a competitor is that you get a completely customized experience. I make dynamic sites that are rich with style and custom features.
-						</div>
-						<div class="text-gray-900 text-left" v-else-if="secondShow" key="1">
-							I also have extensive knowledge using 3rd-party API's such as PayPal, Twitch, Discord, and more. This gives me the oppourtunity to add custom features to your website and interact with other services that you use that would otherwise be unavailable.
-						</div>
-					</transition>
-				</div>
-				<div class="pb-8">
-					<div class="absolute bottom-10 left-10 right-10">
-						<div class="flex justify-center">
-							<button @click="secondShowPrev">
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-900 hover:text-visudo-blue" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
-								</svg>
-							</button>
-							<button @click="secondShowNext">
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-900 hover:text-visudo-blue" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-								</svg>
-							</button>
-						</div>
+			<div class="pb-8">
+				<div class="absolute bottom-10 left-10 right-10">
+					<div class="flex justify-center">
+						<button @click="firstShowPrev">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-visudo-green hover:text-visudo-blue" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+							</svg>
+						</button>
+						<button @click="firstShowNext">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-visudo-green hover:text-visudo-blue" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+							</svg>
+						</button>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="w-3/4 h-5/6 mt-4 mb-10 mx-auto bg-gray-500 border rounded-lg border-gray-900 shadow-lg">
+		<div class="w-3/4 h-5/6 mt-4 mb-10 mx-auto bg-gray-500 border rounded-lg border-gray-900 shadow-lg font-bold">
 			<div class="w-2/3 text-2xl text-visudo-green font-black mb-6 mt-4 border rounded-lg bg-gray-900 border-gray-900 text-center ml-auto mr-auto">
 				What have I done?
 			</div>
@@ -79,46 +53,38 @@
 				</div>
 			</div>
 		</div>
-		<div class="w-2/3 text-3xl text-visudo-green font-black mb-4 mt-4 text-center ml-auto mr-auto">
+		<div class="w-2/3 text-3xl text-visudo-green font-black mb-6 mt-4 text-center ml-auto mr-auto">
 			Things I can create:
 		</div>
-		<div class="ml-auto mr-auto h-60 w-full flex flex-row justify-evenly items-top mb-10 text-center text-gray-900 font-bold sm:h-80">
-			<div class="w-1/6 border border-2 rounded-lg border-gray-900 bg-gray-500">
+		<div class="mx-auto h-60 w-3/4 flex flex-row justify-evenly items-top mb-10 text-center text-gray-900 font-bold sm:h-80">
+			<div class="w-1/4 border border-2 rounded-lg border-gray-900 bg-gray-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
 				<div class="ml-auto mr-auto mt-4 md:text-lg mb-4">Node.js Micro-services</div>
 				<img src="../static/Node.js_logo.svg" class="m-auto w-2/3 h-2/3"/>
 			</div>
-			<div class="w-1/6 border border-2 rounded-lg border-gray-900 bg-gray-500">
-				<div class="ml-auto mr-auto mt-4 md:text-lg mb-4">A CRUD application</div>
+			<div class="w-1/4 border border-2 rounded-lg border-gray-900 bg-gray-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+				<div class="ml-auto mr-auto mt-4 md:text-lg mb-4">CRUD applications</div>
 				<img src="../static/lara.svg" class="m-auto w-2/3 h-2/3"/>
 			</div>
-			<div class="w-1/6 border border-2 rounded-lg border-gray-900 bg-gray-500">
-				<div class="ml-auto mr-auto mt-4 md:text-lg mb-4">A Single Page Application</div>
+			<div class="w-1/4 border border-2 rounded-lg border-gray-900 bg-gray-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+				<div class="ml-auto mr-auto mt-4 md:text-lg mb-4">Single Page Applications</div>
 				<img src="../static/Vue.js_Logo_2.svg" class="m-auto w-2/3 h-2/3"/>
 			</div>
-			<div class="w-1/6 border border-2 rounded-lg border-gray-900 bg-gray-500">
-				<div class="ml-auto mr-auto mt-4 md:text-lg mb-4">Any other project ideas</div>
-				<img src="../static/idee.svg" class="m-auto w-2/3 h-2/3"/>
-			</div>
 		</div>
-		<div class="w-2/3 text-3xl text-visudo-green font-black mb-4 mt-4 text-center ml-auto mr-auto">
+		<div class="w-2/3 text-3xl text-visudo-green font-black mb-6 mt-4 text-center ml-auto mr-auto">
 			Services I can integrate:
 		</div>
-		<div class="ml-auto mr-auto h-60 w-full flex flex-row justify-evenly items-top mb-10 text-center text-gray-900 font-bold sm:h-80">
-			<div class="w-1/6 border border-2 rounded-lg border-gray-900 bg-gray-500">
+		<div class="mx-auto h-60 w-3/4 flex flex-row justify-evenly items-top mb-10 text-center text-gray-900 font-bold sm:h-80">
+			<div class="w-1/4 border border-2 rounded-lg border-gray-900 bg-gray-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
 				<div class="ml-auto mr-auto mt-4 text-lg mb-4">Discord</div>
 				<img src="../static/js_discord-ar21.svg" class="m-auto w-2/3 h-2/3"/>
 			</div>
-			<div class="w-1/6 border border-2 rounded-lg border-gray-900 bg-gray-500">
+			<div class="w-1/4 border border-2 rounded-lg border-gray-900 bg-gray-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
 				<div class="ml-auto mr-auto mt-4 text-lg mb-4">Twitch</div>
 				<img src="../static/Twitch_logo.svg" class="m-auto w-2/3 h-2/3"/>
 			</div>
-			<div class="w-1/6 border border-2 rounded-lg border-gray-900 bg-gray-500">
+			<div class="w-1/4 border border-2 rounded-lg border-gray-900 bg-gray-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
 				<div class="ml-auto mr-auto mt-4 text-lg mb-16">GitHub</div>
 				<img src="../static/GitHub_Logo.png" class="m-auto"/>
-			</div>
-			<div class="w-1/6 border border-2 rounded-lg border-gray-900 bg-gray-500">
-				<div class="ml-auto mr-auto mt-4 text-lg mb-16">3rd-party APIs</div>
-				<img src="../static/Globe_icon.svg" class="m-auto h-1/3 w-1/3"/>
 			</div>
 		</div>
 		<div class="w-2/3 text-3xl text-visudo-green font-black mb-4 mt-4 text-center ml-auto mr-auto">
@@ -138,7 +104,6 @@ export default {
 	data() {
 		return {
 			firstShow: false,
-			secondShow: false,
 			player: null,
 		}
 	},
@@ -148,12 +113,6 @@ export default {
 		},
 		firstShowPrev() {
 			this.firstShow = false
-		},
-		secondShowNext() {
-			this.secondShow = true
-		},
-		secondShowPrev() {
-			this.secondShow = false
 		},
 	},
 	mounted() {
